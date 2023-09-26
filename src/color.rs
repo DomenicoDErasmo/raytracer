f4fed87443be389d7770c28927ddf23e60882fff
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2023-09-25 23:52:46 -0400

    end of chapter 9

--- src/color.rs
+++ src/color.rs
@@ -6,6 +6,10 @@ fn color_to_u8(float: f32, interval: &Interval) -> u8 {
     (((std::u8::MAX as f32 + 1.0) - std::f32::EPSILON) * interval.clamp(float)) as _
 }
 
+pub fn linear_to_gamma(linear_component: f32) -> f32 {
+    linear_component.sqrt()
+}
+
 pub fn write_color(stream: &mut impl std::io::Write, pixel_color: Color, samples_per_pixel: i32) {
     let mut red = pixel_color.x;
     let mut green = pixel_color.y;
@@ -15,6 +19,10 @@ pub fn write_color(stream: &mut impl std::io::Write, pixel_color: Color, samples
     red *= scale;
     green *= scale;
     blue *= scale;
+
+    red = linear_to_gamma(red);
+    green = linear_to_gamma(green);
+    blue = linear_to_gamma(blue);
 
     let intensity = Interval {min: 0.0, max: 1.0 - f32::EPSILON};
 
